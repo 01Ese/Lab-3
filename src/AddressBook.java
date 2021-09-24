c6d0cabb1b67af9e1f8b35461622c6c05abfa6ec
Ese <eseiyamu@cmail.carleton.ca>
2021-09-24 13:33:17 -0400

    This is the First Version.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,33 +1,30 @@
 import java.util.ArrayList;
 public class AddressBook {
 
-    //Create
-    BuddyInfo info1 = new BuddyInfo("Jennie","12 Blueberry Ave","3439652019" );
-    BuddyInfo info2 = new BuddyInfo("Aubrey","45 Blane Dr","5359652045");
-    BuddyInfo info3 = new BuddyInfo("Naomi","76 Knox-dale","2829652088");
-    BuddyInfo info4 = new BuddyInfo("Mark","45 Flag street","2889653067");
-    BuddyInfo info5 = new BuddyInfo("Chris","56 Waterfall Ave", "6139654356");
+    private ArrayList<BuddyInfo> buddiesInfo = new ArrayList<>();
 
     //Put into an array
-    private ArrayList<BuddyInfo> info = new ArrayList<BuddyInfo>();
     private void addBuddy(BuddyInfo newInfo){
-        info.add(newInfo);
+        if (newInfo != null) {
+            buddiesInfo.add(newInfo);
+        }
     }
+
     //Remove from array
     private void removeBuddy(BuddyInfo data){
-        for (BuddyInfo buddy:info){
+        for (BuddyInfo buddy:buddiesInfo){
             if (buddy.equals(data)){
-                info.remove(data);
+                buddiesInfo.remove(data);
             }
-
         }
-
     }
 
     public static void main(String[] args) {
         BuddyInfo newInfo = new BuddyInfo("Ese", "35 Bland street", "5856790930" );
         AddressBook address = new AddressBook();
         address.addBuddy(newInfo);
-        address.removeBuddy(newInfo);
+        BuddyInfo newInfo2 = new BuddyInfo("Audrey", "35 Bland street", "5856790930" );
+        address.addBuddy(newInfo2);
+
     }
 }
